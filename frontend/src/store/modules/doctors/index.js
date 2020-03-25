b31b29be8d95a20ca95b06b9c3f0eb8435b7a274
Pedro Henrique Mognon <phm1993@gmail.com>
2020-03-25 19:45:25 -0300

    Fixing imports

--- frontend/src/store/modules/doctors/index.js
+++ frontend/src/store/modules/doctors/index.js
@@ -1,6 +1,6 @@
-import * as actions from './actions';
-import * as getters from './getters';
-import * as mutations from './mutations';
+import actions from './actions';
+import getters from './getters';
+import mutations from './mutations';
 
 const state = {
   list: [],
