df68c39486b4f705bf6c9c2bbcd2131f96e96090
onluiz <luiz.alberto1723@gmail.com>
2020-03-28 13:59:14 -0300

    Fix some stuff

--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -2,7 +2,7 @@ import * as api from '@/api';
 
 export const login = async ({ commit }, { username, password }) => {
   const loggedUser = await api.auth.login({ username, password });
-  return commit('updateLoggedUser', loggedUser);
+  return commit('updateLoggedUser', { ...loggedUser, password });
 };
 
 export const logout = async ({ commit }) => commit('loggedOut');
