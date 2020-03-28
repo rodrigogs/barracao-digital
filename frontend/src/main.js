08f2359858be4f44ae7e6b5c1392b19699bef607
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-03-28 17:57:41 +0100

    Change relative import to alias `@` for project root

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 import 'normalize.css';
 import Vue from 'vue';
-import VueAnnouncer from "vue-announcer";
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import VueAnnouncer from 'vue-announcer';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
 
 Vue.use(VueAnnouncer, {}, router);
 
@@ -12,5 +12,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app');
