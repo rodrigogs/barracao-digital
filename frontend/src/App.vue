c91df69d53d6f436cae490048a02c1aa81bd4396
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-03-28 21:51:36 +0100

    Add footer component to the application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,21 +2,25 @@
   <div class="app">
     <vue-announcer />
 
-    <Header/>
+    <Header />
 
     <div class="app-content">
       <router-view></router-view>
     </div>
+
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template functional>
+  <footer class="footer">
+    <a href="mailto:emailparacontato@barracao.com.br">emailparacontato@barracao.com.br</a>
+  </footer>
+</template>
+
+<style scoped>
+  .footer {
+    background-color: var(--header-bg-color);
+    padding: 0.75rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .footer > a {
+    color: var(--primary-color);
+  }
+
+  .footer > a:hover {
+    color: var(--primary-color--hover);
+  }
+</style>
