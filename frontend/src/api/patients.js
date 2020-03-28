09d14111a7656415c747a895d0c35e0035aa5fbd
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-03-29 00:05:36 +0100

    Fix problem when trying to sign in a patient
    The endpoint doesn't accept optional fields with empty strings, we must explicitly send null instead of ""

--- frontend/src/api/patients.js
+++ frontend/src/api/patients.js
@@ -1,17 +1,47 @@
-import axios from 'axios';
+const encodePatient = ({
+  name = '',
+  age = '',
+  cep = '',
+  meds = null,
+  allergies = null,
+  covenant = null,
+  hasBeenAssisted = false,
+  phone = '',
+  email = null,
+  whatsapp = null,
+  telegram = null,
+  hangout = null,
+  skype = null,
+  status = null,
+} = {}) => ({
+  name,
+  age,
+  cep,
+  meds: meds || null,
+  allergies: allergies || null,
+  covenant: covenant || null,
+  hasBeenAssisted,
+  phone,
+  email: email || null,
+  whatsapp: whatsapp || null,
+  telegram: telegram || null,
+  hangout: hangout || null,
+  skype: skype || null,
+  status: status || null,
+})
 
 export default (request) => ({
-  async signUpPatient({ patient }) {
-    const { data } = await request.post('/patients', patient);
-    return data;
+  async signUpPatient(patient) {
+    return request
+      .post('/patients', encodePatient(patient))
+      .then(({ data }) => data, (error) => Promise.reject(error));
   },
-  async getPatientByTicket({ ticket }) {
+  async getPatientByTicket(ticket) {
     const { data } = await request.get(`/patients/ticket/${ticket}`);
     return data;
   },
   async getList({ cep }) {
     const { data } = await request.get(`/patients/cep/${cep}`);
-    // const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
     return data;
   },
 });
--- frontend/src/store/modules/patients/actions.js
+++ frontend/src/store/modules/patients/actions.js
@@ -1,3 +1,17 @@
+import { patients as patientsApi } from '@/api';
+
 export const setStepFields = async ({ commit }, stepFields) => {
   commit('setStepFields', stepFields);
 };
+
+export const signUpPatient = ({ commit, getters }) =>
+  patientsApi.signUpPatient(getters.getPatientToSignUp).then(
+    data => {
+      commit('clearForm');
+      return Promise.resolve(data);
+    },
+    error => {
+      // TODO: handle promise rejection
+      return Promise.reject(error);
+    }
+  );
