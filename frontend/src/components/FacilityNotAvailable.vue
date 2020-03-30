de87c739f86112642f7b67286dc04a7ac298ce19
rodrigogs <rodrigo.smscom@gmail.com>
2020-03-30 07:56:41 -0300

    Open links in new pages

--- frontend/src/components/FacilityNotAvailable.vue
+++ frontend/src/components/FacilityNotAvailable.vue
@@ -19,9 +19,9 @@
 
     <h6>O que fazer se estou com SINTOMAS LEVES?</h6>
     <ul>
-      <li>Permaneça em casa por pelo menos 14 dias depois do início dos sintomas, observando as recomendações de isolamento domiciliar para diminuir o contágio entre familiares. Uma boa fonte de informações sobre como diminuir o risco de transmissão dentro de casa é este Guia do Isolamento Domiciliar. <a href="https://g1.globo.com/bemestar/coronavirus/noticia/2020/03/17/guia-do-isolamento-domiciliar-como-preparar-sua-casa-para-conviver-com-suspeitos-de-infeccao-por-coronavirus.ghtml">https://g1.globo.com/bemestar/coronavirus/noticia/2020/03/17/guia-do-isolamento-domiciliar-como-preparar-sua-casa-para-conviver-com-suspeitos-de-infeccao-por-coronavirus.ghtml</a></li><br>
+      <li>Permaneça em casa por pelo menos 14 dias depois do início dos sintomas, observando as recomendações de isolamento domiciliar para diminuir o contágio entre familiares. Uma boa fonte de informações sobre como diminuir o risco de transmissão dentro de casa é este Guia do Isolamento Domiciliar. <a target="_blank" href="https://g1.globo.com/bemestar/coronavirus/noticia/2020/03/17/guia-do-isolamento-domiciliar-como-preparar-sua-casa-para-conviver-com-suspeitos-de-infeccao-por-coronavirus.ghtml">https://g1.globo.com/bemestar/coronavirus/noticia/2020/03/17/guia-do-isolamento-domiciliar-como-preparar-sua-casa-para-conviver-com-suspeitos-de-infeccao-por-coronavirus.ghtml</a></li><br>
       <li>Monitore os seus sintomas e mantenha-se informado pelas fontes oficiais sobre onde procurar ajuda caso apresente piora, definida como sintomas respiratórios + febre (37,8°C ou mais) por mais de 24h e/ou falta de ar.</li><br>
-      <li>Os centros de saúde de referência para sua região podem ser consultados no link: <a href="https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia">https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia</a></li><br>
+      <li>Os centros de saúde de referência para sua região podem ser consultados no link: <a target="_blank" href="https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia">https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia</a></li><br>
       <li>Se precisar ir para o hospital ou centro de saúde, tome os seguintes cuidados:
         <ul>
           <li>Caso possua uma máscara hospitalar, use-a. Senão, procure cobrir a boca e o nariz ao tossir ou espirrar com um lenço descartável, e descarte-o logo após.</li><br>
@@ -34,14 +34,14 @@
 
     <h6>O que fazer se estou com SINTOMAS MODERADOS/GRAVES (sintomas respiratórios + febre de 37,8°C ou mais por mais de 24h e/ou falta de ar)?</h6>
     <p>Dirija-se para o hospital de referência da sua região, que pode ser consultado no link:</p>
-    <a href="https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia">https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia</a>
+    <a target="_blank" href="https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia">https://coronavirus.saude.gov.br/sobre-a-doenca#hospitais-referencia</a>
 
     <h6>Fontes oficiais de informação:</h6>
     <ul>
       <li>Aplicativo Coronavirus-SUS para celular</li><br>
-      <li>Website da Organização Panamericana da Saúde/Organização Mundial da Saúde sobre o coronavírus: <a href="https://www.paho.org/bra/index.php?option=com_content&view=article&id=6101:folha-informativa-novo-coronavirus-2019-ncov&Itemid=875">https://www.paho.org/bra/index.php?option=com_content&view=article&id=6101:folha-informativa-novo-coronavirus-2019-ncov&Itemid=875</a></li><br>
-      <li>Website do Ministério da Saúde sobre o coronavírus: <a href="https://coronavirus.saude.gov.br">https://coronavirus.saude.gov.br</a></li><br>
-      <li>Website da Sociedade Brasileira de Infectologia sobre o coronavírus: <a href="https://www.infectologia.org.br/pg/1567/comunicados-e-notas-da-sbi-referente-ao-novo-coronavrus">https://www.infectologia.org.br/pg/1567/comunicados-e-notas-da-sbi-referente-ao-novo-coronavrus</a></li>
+      <li>Website da Organização Panamericana da Saúde/Organização Mundial da Saúde sobre o coronavírus: <a target="_blank" href="https://www.paho.org/bra/index.php?option=com_content&view=article&id=6101:folha-informativa-novo-coronavirus-2019-ncov&Itemid=875">https://www.paho.org/bra/index.php?option=com_content&view=article&id=6101:folha-informativa-novo-coronavirus-2019-ncov&Itemid=875</a></li><br>
+      <li>Website do Ministério da Saúde sobre o coronavírus: <a target="_blank" href="https://coronavirus.saude.gov.br">https://coronavirus.saude.gov.br</a></li><br>
+      <li>Website da Sociedade Brasileira de Infectologia sobre o coronavírus: <a target="_blank" href="https://www.infectologia.org.br/pg/1567/comunicados-e-notas-da-sbi-referente-ao-novo-coronavrus">https://www.infectologia.org.br/pg/1567/comunicados-e-notas-da-sbi-referente-ao-novo-coronavrus</a></li>
     </ul>
 
   </div>
