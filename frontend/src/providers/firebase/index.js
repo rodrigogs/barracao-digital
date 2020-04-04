e466a6c1411b941017e959e370761f549da0cab9
rodrigogs <rodrigo.smscom@gmail.com>
2020-04-04 13:16:51 -0300

    Finally fixed push notifications

--- frontend/src/providers/firebase/index.js
+++ frontend/src/providers/firebase/index.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase/app';
 import messagingAdapter from './messaging';
-import { FIREBASE_CONFIG } from '../../config';
+import { FIREBASE_CONFIG } from '@/config';
 
 // Firebase config is a base64 encoded file in VUE_APP_FIREBASE_CONFIG environment variable
-const firebaseConfig = JSON.parse(atob(FIREBASE_CONFIG));
+const firebaseConfig = JSON.parse(Buffer.from(FIREBASE_CONFIG, 'base64').toString('utf8'));
 
-export const app = firebase.initializeApp(firebaseConfig);
-export const messaging = messagingAdapter(app);
+firebase.initializeApp(firebaseConfig);
+
+export const messaging = messagingAdapter(firebase);
+export const firestore = firestoreAdapter(firebase);
