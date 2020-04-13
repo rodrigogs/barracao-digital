948fb3d8871c95b500d3472e8f33c232ef94e261
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-04-13 17:36:58 +0200

    Create basic Facilities list layout

--- frontend/pages/manage/facilities.vue
+++ frontend/pages/manage/facilities.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>
-    Facilities list here!
-  </div>
+  <v-container fluid>
+    <v-card elevation="0" append>
+      <v-toolbar color="primary" flat dark dense>
+        <v-toolbar-title>Lista de instalações</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-if="$auth.user.admin"
+          icon
+          title="Criar instalação"
+          @click="create"
+        >
+          <v-icon>mdi-account-plus</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+      <v-divider />
+
+      <v-data-table
+        :headers="headers"
+        :items="facilities"
+        item-key="origin"
+        class="elevation-1"
+      >
+      </v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-  middleware: ['auth', 'isAdminOrMaster']
+  middleware: ['auth', 'isAdminOrMaster'],
+  data: () => ({
+    facilities: [],
+    headers: [
+      { text: 'CEP de origem', value: 'origin' },
+      { text: 'Contato', value: 'contact' },
+      { text: 'Tipo do contato', value: 'contactType' }
+    ],
+    selected: [],
+    isDeleting: false
+  }),
+  computed: {
+    hasSelected() {
+      return this.selected.length > 0
+    },
+
+    selectedFacility() {
+      return this.selected[0]
+    }
+  },
+  methods: {
+    create() {},
+    edit() {},
+    remove() {}
+  }
 }
 </script>
