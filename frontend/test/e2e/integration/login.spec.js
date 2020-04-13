1a61648ab07be18fb4cbd7e69f67b2db27ee002b
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-04-13 15:32:37 +0200

    Test doctor login

---
+++ frontend/test/e2e/integration/login.spec.js
@@ -0,0 +1,23 @@
+describe('Login screen', () => {
+  it('Logs in a doctor', () => {
+    cy.server()
+    cy.fixture('patients').as('patientsJSON')
+    cy.route('GET', `${Cypress.env('apiUrl')}/patients/**`, '@patientsJSON').as(
+      'getPatients'
+    )
+
+    cy.visit('/login')
+
+    cy.get('input[type="text"]').type(Cypress.env('username'))
+    cy.get('input[type="password"]').type(Cypress.env('password'))
+
+    cy.get('button.primary').click()
+
+    cy.location('pathname', { timeout: 10000 }).should('eq', '/doctor/')
+
+    cy.wait('@getPatients').then(({ response }) => {
+      const patients = response.body.items
+      cy.get('tbody > tr').should('have.length', patients.length)
+    })
+  })
+})
