d9a6d26d93d47c15399c1e7d727608c6e8e24a86
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-04-13 20:01:54 +0200

    Add construction page to the doctors module

--- frontend/pages/manage/doctors.vue
+++ frontend/pages/manage/doctors.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    Doctors list here!
-  </div>
+  <v-banner width="100%" max-width="600px" single-line>
+    <v-icon slot="icon" color="warning" size="36">
+      mdi-wrench
+    </v-icon>
+    <span>Esta página está em construção</span>
+
+    <template v-slot:actions>
+      <v-btn color="primary" text @click="$router.go(-1)">
+        Voltar
+      </v-btn>
+    </template>
+  </v-banner>
 </template>
 
 <script>
