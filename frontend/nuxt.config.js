949afa934f60be6437142946e6a3d49e4b464f2a
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-04-13 17:11:26 +0200

    Add proper viewport meta to the frontend app

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -20,7 +20,11 @@ export default {
     title: 'Barracao Digital',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui'
+      },
       {
         hid: 'description',
         name: 'description',
