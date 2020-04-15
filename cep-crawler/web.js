e9393cd6747c43e65359bb73e437b5c0bce329da
rodrigogs <rodrigo.smscom@gmail.com>
2020-04-15 15:19:14 -0300

    Added /address path

--- cep-crawler/web.js
+++ cep-crawler/web.js
@@ -4,7 +4,12 @@ const app = new Koa();
 const cepCrawler = require('.');
 
 app.use(async ctx => {
-  const address = ctx.path.substr(1);
+  if (!ctx.path.startsWith('/address')) {
+    ctx.status = 404;
+    ctx.body = 'Not Found';
+    return;
+  }
+  const address = ctx.path.replace('/address', '');
   if (address.length < 1) return (ctx.body = 'Invalid parameter');
   const ceps = await cepCrawler(address);
   ctx.body = ceps.join(', ');
