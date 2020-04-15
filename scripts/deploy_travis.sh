0616336cbf34e85ef7edc8a86c83173b5e54b3f8
rodrigogs <rodrigo.smscom@gmail.com>
2020-04-15 01:57:32 -0300

    Fixed travis

--- scripts/deploy_travis.sh
+++ scripts/deploy_travis.sh
@@ -28,7 +28,7 @@ if [[ $DEPLOY_FRONTEND == "true" ]]; then
     echo -e "${BOLD}${YELLOW}Deploying frontend...${RESET}"
     npm run deploy:frontend
     echo -e "${BOLD}${YELLOW}Crlearing frontend cache...${RESET}"
-    nodejs || node ./scripts/invalidate-cloudfront-cache.js
+    nodejs || node ./scripts/invalidate-cloudfront-cache.js web.barracaodigital.com dev.barracaodigital.com
 fi
 
 if [[ $DEPLOY_BACKEND == "true" ]]; then
