59e65d2fead2d6a425a56faecb896d84da0dd22d
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-04-19 20:24:50 +0200

    Create cypress command to register a patient

--- frontend/test/e2e/support/commands.js
+++ frontend/test/e2e/support/commands.js
@@ -1,28 +1,40 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import faker from 'faker/locale/pt_BR'
+import cpf from 'cpf'
+
+Cypress.Commands.add('registerPatient', ({ cep = '55555-550' } = {}) => {
+  const randomWords = faker.random
+    .words(3)
+    .split(' ')
+    .join(', ')
+  const phone = faker.phone.phoneNumber('(##) ####-####')
+
+  cy.visit('/screening-room')
+
+  cy.contains('.v-card__text', 'Termos de uso para utilizadores')
+  cy.get('.v-card__actions > .v-btn').click()
+
+  cy.get('input[id="name"]').type(faker.name.findName())
+  cy.get('input[id="age"]').type(faker.random.number(120))
+  cy.get('input[id="cpf"]').type(cpf.generate())
+  cy.get('input[id="cep"]').type(cep)
+
+  cy.get('button#myDataBtn').click()
+
+  cy.get('input[id="meds"]').type(randomWords)
+  cy.get('input[id="allergies"]').type(randomWords)
+  cy.get('input[id="covenant"]').type('UNIMED')
+
+  cy.get('button#medicalInformationsBtn').click()
+
+  cy.get('input[id="phone"]').type(phone)
+  cy.get('input[id="email"]').type(faker.internet.email())
+  cy.get('input[id="whatsapp"]').type(phone)
+  cy.get('input[id="telegram"]').type(phone)
+  cy.get('input[id="hangout"]').type(phone)
+  cy.get('input[id="skype"]').type(faker.internet.userName())
+
+  cy.get('button#contactBtn').click()
+})
 
 Cypress.Commands.add(
   'login',
