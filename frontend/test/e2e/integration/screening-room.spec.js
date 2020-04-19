7a9c447f883481006212cf1dc1fa7d09b6942ad5
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-04-19 20:25:12 +0200

    Test patient screening with unexisting CEP

--- frontend/test/e2e/integration/screening-room.spec.js
+++ frontend/test/e2e/integration/screening-room.spec.js
@@ -1,40 +1,6 @@
-import faker from 'faker/locale/pt_BR'
-import cpf from 'cpf'
-
 describe('Screening rooom screen', () => {
   it('Register a patient with existing CEP', () => {
-    const randomWords = faker.random
-      .words(3)
-      .split(' ')
-      .join(', ')
-    const phone = faker.phone.phoneNumber('(##) ####-####')
-
-    cy.visit('/screening-room')
-
-    cy.contains('.v-card__text', 'Termos de uso para utilizadores')
-    cy.get('.v-card__actions > .v-btn').click()
-
-    cy.get('input[id="name"]').type(faker.name.findName())
-    cy.get('input[id="age"]').type(faker.random.number(120))
-    cy.get('input[id="cpf"]').type(cpf.generate())
-    cy.get('input[id="cep"]').type('55555-550')
-
-    cy.get('button#myDataBtn').click()
-
-    cy.get('input[id="meds"]').type(randomWords)
-    cy.get('input[id="allergies"]').type(randomWords)
-    cy.get('input[id="covenant"]').type('UNIMED')
-
-    cy.get('button#medicalInformationsBtn').click()
-
-    cy.get('input[id="phone"]').type(phone)
-    cy.get('input[id="email"]').type(faker.internet.email())
-    cy.get('input[id="whatsapp"]').type(phone)
-    cy.get('input[id="telegram"]').type(phone)
-    cy.get('input[id="hangout"]').type(phone)
-    cy.get('input[id="skype"]').type(faker.internet.userName())
-
-    cy.get('button#contactBtn').click()
+    cy.registerPatient({ cep: '55555-550' })
 
     cy.location('pathname', { timeout: 10000 })
       .should((pathname) => {
@@ -45,6 +11,22 @@ describe('Screening rooom screen', () => {
         const ticket = pathname.split('/')[2]
         cy.contains('p', 'Sua senha de retorno é')
         cy.contains('p', ticket)
+      })
+  })
+
+  it('Register a patient with UNexisting CEP', () => {
+    cy.registerPatient({ cep: '11111-111' })
+
+    cy.location('pathname', { timeout: 10000 })
+      .should((pathname) => {
+        const ticket = pathname.split('/')[2]
+        expect(pathname).to.eq(`/patient/${ticket}`)
+      })
+      .then(() => {
+        cy.contains(
+          'h3',
+          'Infelizmente não encontramos nenhum barracão digital disponível para a sua área.'
+        )
       })
   })
 })
