83c10a064ac486d385f9355add1fa3fac9cf637d
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2020-06-11 04:44:54 -0300

    Data lake fixes

--- functions/processors/doctors-datalake-processor.js
+++ functions/processors/doctors-datalake-processor.js
@@ -14,35 +14,40 @@ const ensureMap = (image, prop) => image && (image[prop] ? image[prop].M : null)
 export const handler = (event, context, callback) => {
   firehose.putRecordBatch({
     DeliveryStreamName: DOCTORS_DELIVERY_STREAM,
-    Records: event.Records.map((record) => ({
-      Data: JSON.stringify({
-        eventName: String(record.eventName),
-        id: ensureString(record.dynamodb.NewImage, 'id'),
-        username: ensureString(record.dynamodb.NewImage, 'username'),
-        name: ensureString(record.dynamodb.NewImage, 'name'),
-        sort: ensureString(record.dynamodb.NewImage, 'sort'),
-        specialty: ensureString(record.dynamodb.NewImage, 'specialty'),
-        active: ensureBoolean(record.dynamodb.NewImage, 'active'),
-        admin: ensureBoolean(record.dynamodb.NewImage, 'admin'),
-        master: ensureBoolean(record.dynamodb.NewImage, 'master'),
-        cep: ensureString(record.dynamodb.NewImage, 'cep'),
-        crm: ensureString(record.dynamodb.NewImage, 'crm'),
-        email: ensureBoolean(record.dynamodb.NewImage, 'email'),
-        fu: ensureString(record.dynamodb.NewImage, 'fu'),
-        textSessions: Object
-          .keys(ensureMap(record.dynamodb.NewImage, 'textSessions') || {})
-          .join(','),
-        videoSessions: Object
-          .keys(ensureMap(record.dynamodb.NewImage, 'videoSessions') || {})
-          .join(','),
-        createdAt: ensureNumber(record.dynamodb.NewImage, 'createdAt'),
-        updatedAt: ensureNumber(record.dynamodb.NewImage, 'updatedAt'),
-        version: ensureNumber(record.dynamodb.NewImage, 'version'),
-        approximateCreationDateTime: Number(record.dynamodb.ApproximateCreationDateTime),
-        sequenceNumber: String(record.dynamodb.SequenceNumber),
-        ingestedAt: Number(Date.now()),
-      }),
-    })),
+    Records: event.Records.map((record) => {
+      const image = record.eventName === 'REMOVE'
+        ? record.dynamodb.OldImage
+        : record.dynamodb.NewImage;
+      return {
+        Data: JSON.stringify({
+          eventName: String(record.eventName),
+          id: ensureString(image, 'id'),
+          username: ensureString(image, 'username'),
+          name: ensureString(image, 'name'),
+          sort: ensureString(image, 'sort'),
+          specialty: ensureString(image, 'specialty'),
+          active: ensureBoolean(image, 'active'),
+          admin: ensureBoolean(image, 'admin'),
+          master: ensureBoolean(image, 'master'),
+          cep: ensureString(image, 'cep'),
+          crm: ensureString(image, 'crm'),
+          email: ensureBoolean(image, 'email'),
+          fu: ensureString(image, 'fu'),
+          textSessions: Object
+            .keys(ensureMap(image, 'textSessions') || {})
+            .join(','),
+          videoSessions: Object
+            .keys(ensureMap(image, 'videoSessions') || {})
+            .join(','),
+          createdAt: ensureNumber(image, 'createdAt'),
+          updatedAt: ensureNumber(image, 'updatedAt'),
+          version: ensureNumber(image, 'version'),
+          approximateCreationDateTime: Number(record.dynamodb.ApproximateCreationDateTime),
+          sequenceNumber: String(record.dynamodb.SequenceNumber),
+          ingestedAt: Number(Date.now()),
+        }),
+      };
+    }),
   }, (error) => {
     if (error) return callback(error);
     return callback(null);
--- functions/processors/facilities-datalake-processor.js
+++ functions/processors/facilities-datalake-processor.js
@@ -13,24 +13,29 @@ const ensureBoolean = (image, prop) => image && (image[prop] ? image[prop].BOOL 
 export const handler = (event, context, callback) => {
   firehose.putRecordBatch({
     DeliveryStreamName: FACILITIES_DELIVERY_STREAM,
-    Records: event.Records.map((record) => ({
-      Data: JSON.stringify({
-        eventName: String(record.eventName),
-        name: ensureString(record.dynamodb.NewImage, 'name'),
-        origin: ensureString(record.dynamodb.NewImage, 'origin'),
-        destination: ensureString(record.dynamodb.NewImage, 'destination'),
-        techDirector: ensureString(record.dynamodb.NewImage, 'techDirector'),
-        type: ensureString(record.dynamodb.NewImage, 'type'),
-        active: ensureBoolean(record.dynamodb.NewImage, 'active'),
-        contact: ensureString(record.dynamodb.NewImage, 'contact'),
-        createdAt: ensureNumber(record.dynamodb.NewImage, 'createdAt'),
-        updatedAt: ensureNumber(record.dynamodb.NewImage, 'updatedAt'),
-        version: ensureNumber(record.dynamodb.NewImage, 'version'),
-        approximateCreationDateTime: Number(record.dynamodb.ApproximateCreationDateTime),
-        sequenceNumber: String(record.dynamodb.SequenceNumber),
-        ingestedAt: Number(Date.now()),
-      }),
-    })),
+    Records: event.Records.map((record) => {
+      const image = record.eventName === 'REMOVE'
+        ? record.dynamodb.OldImage
+        : record.dynamodb.NewImage;
+      return ({
+        Data: JSON.stringify({
+          eventName: String(record.eventName),
+          name: ensureString(image, 'name'),
+          origin: ensureString(image, 'origin'),
+          destination: ensureString(image, 'destination'),
+          techDirector: ensureString(image, 'techDirector'),
+          type: ensureString(image, 'type'),
+          active: ensureBoolean(image, 'active'),
+          contact: ensureString(image, 'contact'),
+          createdAt: ensureNumber(image, 'createdAt'),
+          updatedAt: ensureNumber(image, 'updatedAt'),
+          version: ensureNumber(image, 'version'),
+          approximateCreationDateTime: Number(record.dynamodb.ApproximateCreationDateTime),
+          sequenceNumber: String(record.dynamodb.SequenceNumber),
+          ingestedAt: Number(Date.now()),
+        }),
+      });
+    }),
   }, (error) => {
     if (error) return callback(error);
     return callback(null);
