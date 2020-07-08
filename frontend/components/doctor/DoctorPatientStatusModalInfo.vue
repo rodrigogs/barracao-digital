0b1193bcdfdcb30ccbe9b3cc11fb947e2a170492
franco <francobrunoc@gmail.com>
2020-07-08 08:35:45 -0300

    #85 - A rule was added to the chat windows as the doctor which is currently attending is the only allowed to open the conversation.

--- frontend/components/doctor/DoctorPatientStatusModalInfo.vue
+++ frontend/components/doctor/DoctorPatientStatusModalInfo.vue
@@ -29,7 +29,7 @@
       <DoctorPatientStatusModalSummary :patient="patient" />
 
       <v-stepper
-        v-if="!isUnassisted && !isFinishedStatus"
+        v-if="(!isUnassisted && !isFinishedStatus && (patient.textSession === this.$auth.user.username || !patient.textSession))"
         v-model="step"
         vertical
       >
