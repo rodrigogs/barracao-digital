2b774069148276585f024a8fb9f2d66d1b552815
franco <francobrunoc@gmail.com>
2020-07-21 12:54:55 -0300

    #81 - Base64 variable generation and decoding for the assetlinks (Google Play TWA) signature on building.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -25,6 +25,8 @@ concurrently\
   "cd $script_dir/../lib/providers/firebase && rm -rf package-lock.json"\
   "cd $script_dir/../lib/providers/telegram && rm -rf node_modules"\
   "cd $script_dir/../lib/providers/telegram && rm -rf package-lock.json"\
+  "cd $script_dir/../lib/providers/sms && rm -rf node_modules"\
+  "cd $script_dir/../lib/providers/sms && rm -rf package-lock.json"\
   "cd $script_dir/../functions && rm -rf node_modules"\
   "cd $script_dir/../functions && rm -rf package-lock.json"\
   "cd $script_dir/../functions/processors && rm -rf node_modules"\
