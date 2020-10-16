da66a84315ca4e1d2563d2a2de097ae6dd6c2372
francobrunoc <francobrunoc@gmail.com>
2020-10-16 10:05:17 -0300

    Reverting false commit.

--- _nuxt/f9bc493.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{781:function(r,d,o){"use strict";o.r(d);var t={name:"redcap",created:function(){window.open("http://redcap.cardiologia.org.br/redcap_v10.1.1/DataEntry/record_status_dashboard.php?pid=108&rd_id="),this.$router.back()}},e=o(55),component=Object(e.a)(t,void 0,void 0,!1,null,null,null);d.default=component.exports}}]);
